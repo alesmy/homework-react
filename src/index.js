8e7cdf5aa624468c966d78175c220346a357dab0
alexandraSS <alexa434343@gmail.com>
2021-11-06 15:31:15 +0300

    дз урока 1

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const MessageComponent = ({ message }) => {
+  return <div class='message'>
+    Вывод сообщения из пропса: <b>{message}</b>
+  </div >
+}
+
+const App = ({ ...rest }) => {
+  return (
+    <div>
+      <h1>Сообщение:</h1>
+
+      <MessageComponent {...rest} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App message={'Какой-то длинный текст...'} />, document.getElementById('root'));
